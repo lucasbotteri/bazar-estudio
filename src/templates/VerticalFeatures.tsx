962030425334ea1799b5e9b1caab2f60bc331a1c
Lucas Botteri <lucasbotteri@hotmail.com>
2021-07-13 22:53:31 -0300

    removed vertical features

--- src/templates/VerticalFeatures.tsx
+++
@@ -1,9 +0,0 @@
-import { Section } from '../layout/Section';
-
-const VerticalFeatures = () => (
-  <Section title="Mira algunos de nuestros productos">
-    <></>
-  </Section>
-);
-
-export { VerticalFeatures };
